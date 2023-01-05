76185694255513e077c5766ed02f27f1fd1fde89
Amiel Brencis Salipande <absalipande@gmail.com>
2023-01-05 14:19:19 +0800

    fix: return stringify

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -13,14 +13,19 @@ const AddUser = () => {
   const saveNewUser = async (event) => {
     event.preventDefault();
 
-    const data = {
-      name: userName,
-      age: userAge,
-      email: userEmail,
-      gender: userGender,
-    }
+    const data = JSON.parse(
+      JSON.stringify({
+        name: userName,
+        age: userAge,
+        email: userEmail,
+        gender: userGender,
+      })
+    );
     try {
-      await axios.post('https://mern-app-backend-x1cm.onrender.com/users', data);
+      await axios.post(
+        'https://mern-app-backend-x1cm.onrender.com/users',
+        data
+      );
       navigate('/');
     } catch (error) {
       console.log(error);
@@ -28,68 +33,68 @@ const AddUser = () => {
   };
 
   return (
-    <div className="columns mt-5">
-      <div className="column is-half">
+    <div className='columns mt-5'>
+      <div className='column is-half'>
         <form onSubmit={saveNewUser}>
-          <div className="field">
-            <label className="label">Name</label>
-            <div className="control">
+          <div className='field'>
+            <label className='label'>Name</label>
+            <div className='control'>
               <input
-                type="text"
-                className="input"
-                placeholder="Name"
+                type='text'
+                className='input'
+                placeholder='Name'
                 value={userName}
                 onChange={(event) => setUserName(event.target.value)}
               />
             </div>
           </div>
-          <div className="field">
-            <label className="label">Age</label>
-            <div className="control">
+          <div className='field'>
+            <label className='label'>Age</label>
+            <div className='control'>
               <input
-                type="number"
-                className="input"
-                placeholder="Age"
+                type='number'
+                className='input'
+                placeholder='Age'
                 value={userAge}
                 onChange={(event) => setUserAge(event.target.value)}
               />
             </div>
           </div>
-          <div className="field">
-            <label className="label">Email</label>
-            <div className="control">
+          <div className='field'>
+            <label className='label'>Email</label>
+            <div className='control'>
               <input
-                type="text"
-                className="input"
-                placeholder="Email"
+                type='text'
+                className='input'
+                placeholder='Email'
                 value={userEmail}
                 onChange={(event) => setUserEmail(event.target.value)}
               />
             </div>
           </div>
-          <div className="field">
-            <label className="label">Gender</label>
-            <div className="control">
-              <div className="select is-fullwidth">
+          <div className='field'>
+            <label className='label'>Gender</label>
+            <div className='control'>
+              <div className='select is-fullwidth'>
                 <select
                   value={userGender}
                   onChange={(event) => setUserGender(event.target.value)}
                 >
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                  <option value="Non-Binary">Non-Binary</option>
-                  <option value="Transgender">Transgender</option>
-                  <option value="Intersex">Intersex</option>
-                  <option value="I prefer not to say">
+                  <option value='Male'>Male</option>
+                  <option value='Female'>Female</option>
+                  <option value='Non-Binary'>Non-Binary</option>
+                  <option value='Transgender'>Transgender</option>
+                  <option value='Intersex'>Intersex</option>
+                  <option value='I prefer not to say'>
                     I prefer not to say
                   </option>
                 </select>
               </div>
             </div>
           </div>
-          <div className="field">
-            <div className="control">
-              <button className="button is-success" type="submit">
+          <div className='field'>
+            <div className='control'>
+              <button className='button is-success' type='submit'>
                 Save
               </button>
             </div>
--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -28,12 +28,14 @@ const EditUser = () => {
   const updateUser = async (event) => {
     event.preventDefault();
 
-    const editData = {
-      name: userName,
-      age: userAge,
-      email: userEmail,
-      gender: userGender,
-    };
+    const editData = JSON.parse(
+      JSON.stringify({
+        name: userName,
+        age: userAge,
+        email: userEmail,
+        gender: userGender,
+      })
+    );
     try {
       await axios.patch(
         `https://mern-app-backend-x1cm.onrender.com/users/${id}`,
